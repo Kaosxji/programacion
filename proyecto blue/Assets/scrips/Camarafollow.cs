ffd04efa328e9dadf4faf68d571cb10da4966364
Kaosxji <142038918+Kaosxji@users.noreply.github.com>
2023-08-11 12:17:33 -0500

    corrección de programa
    este es el programa correcto

---
+++ proyecto blue/Assets/scrips/Camarafollow.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camarafollow : MonoBehaviour
+{
+    //Atrivuto Publico
+    public Transform pelota;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(
+            pelota.position.x,
+            pelota.position.y,
+            -1);  
+    }
+}
