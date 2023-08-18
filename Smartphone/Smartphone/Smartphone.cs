b19b775b47f4f39462094c7747b83e9f1b5a5d2d
Kaosxji <142038918+Kaosxji@users.noreply.github.com>
2023-08-18 11:15:48 -0600

    nuevo programa

---
+++ Smartphone/Smartphone/Smartphone.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smartphone
+{
+    class Smartphone
+    {
+        public string NetTech;
+        public Launch Launch;
+        public float Body;
+        public float Display;
+        public int Plataform;
+        public int Memory;
+        public int MainCamara;
+        public int SelfieCam;
+        public int Sound;
+        public int Comms;
+        public int Features;
+        public int Battery;
+        public int Mic;
+
+    }
+}
