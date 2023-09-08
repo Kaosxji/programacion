86b6963a70ba33708a9cff72a4707edb6e55c685
Kaosxji <142038918+Kaosxji@users.noreply.github.com>
2023-09-08 11:54:15 -0600

    Nuevoproyecto Nombres Lista

---
+++ NombresEnArreglo/GeneradorDeNombres.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NombresEnArreglo
+{
+    class GeneradorDeNombres
+    {
+        string[] InicialDetuNombre = {" Asmud/Aslaug "," Bjord/Belinda "," Cade/Cadence ",
+         " Dagiff/Dagmar "," Edan/Edda "," Floki/Freja "," Georg/Grete "," Hvitserk/Hege ",
+         " Ivar/Ingrid "," Jorge/Jytte "," Kennet/Katerin "," Leif/Largertha "," Magnua/Magda ",
+         " Niklaus/Nadia ", " Olaf/Ophelia "," Knute/Karan "," Laerke/Frederik "," Ragnar/Ragnild",
+         " Sigurd/Siggy "," Thor/Torvi ", " Ubbe/Uma "," Vilhelm/Valda","Viggo/Astrid","Rollo/Rhea",
+         "Yngvard"
+        }  
+    }
+}
---
+++ Practica2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Ciudades = {"Osaka", "Tokyo","Seul",
+             "Puebla", "Montreal","paris","Londres","Okinawa"
+            };
+
+            int cantElems = Ciudades.Length;
+            Console.WriteLine(" El arreglo tiene " + cantElems + " elemnetos ");
+            
+
+            //acceder a los elementos del arreglo
+            string segundaCiudad = Ciudades[1];
+            string ciudad4 = Ciudades[4];
+            Console.WriteLine(segundaCiudad);
+            Console.WriteLine(ciudad4);
+            Console.WriteLine(Ciudades[0]);
+            Console.WriteLine(Ciudades[5]);
+
+            //Modifico elemento de un arreglo
+            Ciudades[4] = "Otawa";
+            Console.WriteLine(Ciudades[4]);
+            for(int i = 0; i < 8; i++) 
+            {//Esta Hardcodeado el 8
+
+            }
+            for (int i = 0; i < Ciudades.Length; i++)
+            {//Parametrizado
+                Console.WriteLine(i + ".-" + Ciudades[i]);
+            }
+            Console.Read();
+        }
+    }
+}
