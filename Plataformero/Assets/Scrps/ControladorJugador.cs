1f744f79dca4aa39219f7fc199c4bfe66aa63934
Kaosxji <142038918+Kaosxji@users.noreply.github.com>
2023-09-12 14:42:16 -0600

    Controles

--- Plataformero/Assets/Scrps/ControladorJugador.cs
+++ Plataformero/Assets/Scrps/ControladorJugador.cs
@@ -4,21 +4,40 @@ using UnityEngine;
 
 public class ControladorJugador : MonoBehaviour
 {
-    private Rigidbody2D MiCuerpo;
-    private Animator MiAnimator;
+    public float velocidadCaminar = 5;
+    private Rigidbody2D miCuerpo;
+    private Animator miAnimator;
     // Start is called before the first frame update
     void Start()
     {
-        MiAnimator = GetComponent<Animator>();
-        MiCuerpo = GetComponent<Rigidbody2D>();
+        miAnimator = GetComponent<Animator>();
+        miCuerpo = GetComponent<Rigidbody2D>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        float velVert = MiCuerpo.velocity.y;
-        MiCuerpo.velocity = new Vector3(5, velVert, 0);
-        MiAnimator.SetBool("Caminata", true);
+        float velVert = miCuerpo.velocity.y;
+        float movHoriz = Input.GetAxis("Horizontal");
+        if (movHoriz > 0)
+        {//se mueve a la derecha
+            transform.rotation =
+                Quaternion.Euler(0, 0, 0);
+            miCuerpo.velocity = new Vector3(velocidadCaminar, velVert, 0);
+            miAnimator.SetBool("Caminata", true);
+        }
+        else if (movHoriz < 0)
+        {//se mueve a la izquierda 
+            transform.rotation =
+                Quaternion.Euler(0, 180, 0);
+            miCuerpo.velocity = new Vector3(-velocidadCaminar, velVert, 0);
+            miAnimator.SetBool("Caminata", true);
+        }
+        else
+        {//no se mueve 
+            miCuerpo.velocity = new Vector3(0, velVert, 0);
+            miAnimator.SetBool("Caminata", false);
+        }
     }
 }
