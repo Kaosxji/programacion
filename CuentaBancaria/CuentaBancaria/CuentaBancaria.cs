4213f14305e9a5a366c494631ee6c5929416e6c5
Kaosxji <142038918+Kaosxji@users.noreply.github.com>
2023-09-12 13:54:30 -0600

    cuenta bancaria pendiente

---
+++ CuentaBancaria/CuentaBancaria/CuentaBancaria.cs
@@ -0,0 +1,23 @@
+﻿namespace CuentaBancaria
+{
+    class CuentaBancaria
+    {
+        private int saldo;
+        private int pin;
+
+        public int getPin()
+        {
+            return pin;
+        }
+        public int setPin(int valor)
+        {
+            pin = valor;
+        }
+        public int getSaldo()
+        {
+            return saldo;
+        }
+
+
+    }
+}
