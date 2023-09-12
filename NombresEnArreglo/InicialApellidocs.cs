53dc5fce6435dd85a4f76f7807921d4866aa8f28
Kaosxji <142038918+Kaosxji@users.noreply.github.com>
2023-09-12 00:44:11 -0600

    se agrego una clase mas

---
+++ NombresEnArreglo/InicialApellidocs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NombresEnArreglo
+{
+    class InicialApellidocs
+    {
+        string[] SegundoNomre = { " The Bold ", " IronSide ", " The Red ", " The Braggart ", " The Crow ", " Halfhand "
+                ," The Innicent ", " The Fearless ", " Hollowwleg ", " Jorgansen ", " Bearclaw ", " The Blackthorn ",
+            " Whitemane ", " Ravencrone "," Swiftseer ", " The Rotten ", " The Slayer ", " Rockbreaker ", " Stoutspear ",
+            " The Smith ", " Unminded ", " The Laggard ", " Windrimer ", " The Pickled ", " The Younger ", "The Broeken"
+        };
+    }
+}
--- NombresEnArreglo/InicialDeNombre.cs
+++ NombresEnArreglo/InicialDeNombre.cs
@@ -6,14 +6,14 @@ using System.Threading.Tasks;
 
 namespace NombresEnArreglo
 {
-    class GeneradorDeNombres
+    class InicialDeNombre
     {
-        string[] InicialDetuNombre = {" Asmud/Aslaug "," Bjord/Belinda "," Cade/Cadence ",
+        string[] PrimerNombre = {" Asmud/Aslaug "," Bjord/Belinda "," Cade/Cadence ",
          " Dagiff/Dagmar "," Edan/Edda "," Floki/Freja "," Georg/Grete "," Hvitserk/Hege ",
          " Ivar/Ingrid "," Jorge/Jytte "," Kennet/Katerin "," Leif/Largertha "," Magnua/Magda ",
          " Niklaus/Nadia ", " Olaf/Ophelia "," Knute/Karan "," Laerke/Frederik "," Ragnar/Ragnild",
          " Sigurd/Siggy "," Thor/Torvi ", " Ubbe/Uma "," Vilhelm/Valda","Viggo/Astrid","Rollo/Rhea",
          "Yngvard"
-        }  
+        };  
     }
 }
